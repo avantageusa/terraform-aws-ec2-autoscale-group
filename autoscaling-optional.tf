3c597e8603a3dd9077cdc91086ce097ae00531cb
atanas-lazarov <58036144+atanas-lazarov@users.noreply.github.com>
2020-03-31 18:41:18 +0300

    Update and rename autoscaling.tf to autoscaling-optional.tf

--- autoscaling-optional.tf
+++ autoscaling-optional.tf
@@ -1,32 +1,30 @@
-# https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/ec2-metricscollected.html
-
 locals {
   autoscaling_enabled = var.enabled && var.autoscaling_policies_enabled ? true : false
 }
 
 resource "aws_autoscaling_policy" "scale_up" {
   count                  = local.autoscaling_enabled ? 1 : 0
-  name                   = "${module.label.id}${var.delimiter}scale${var.delimiter}up"
+  name                   = "${var.name}-scale-up"
   scaling_adjustment     = var.scale_up_scaling_adjustment
   adjustment_type        = var.scale_up_adjustment_type
   policy_type            = var.scale_up_policy_type
   cooldown               = var.scale_up_cooldown_seconds
-  autoscaling_group_name = join("", aws_autoscaling_group.default.*.name)
+  autoscaling_group_name = join("", aws_autoscaling_group.this.*.name)
 }
 
 resource "aws_autoscaling_policy" "scale_down" {
   count                  = local.autoscaling_enabled ? 1 : 0
-  name                   = "${module.label.id}${var.delimiter}scale${var.delimiter}down"
+  name                   = "${var.name}-scale-down"
   scaling_adjustment     = var.scale_down_scaling_adjustment
   adjustment_type        = var.scale_down_adjustment_type
   policy_type            = var.scale_down_policy_type
   cooldown               = var.scale_down_cooldown_seconds
-  autoscaling_group_name = join("", aws_autoscaling_group.default.*.name)
+  autoscaling_group_name = join("", aws_autoscaling_group.this.*.name)
 }
 
 resource "aws_cloudwatch_metric_alarm" "cpu_high" {
   count               = local.autoscaling_enabled ? 1 : 0
-  alarm_name          = "${module.label.id}${var.delimiter}cpu${var.delimiter}utilization${var.delimiter}high"
+  alarm_name          = "${var.name}-cpu-utilization-high"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = var.cpu_utilization_high_evaluation_periods
   metric_name         = "CPUUtilization"
@@ -36,7 +34,7 @@ resource "aws_cloudwatch_metric_alarm" "cpu_high" {
   threshold           = var.cpu_utilization_high_threshold_percent
 
   dimensions = {
-    AutoScalingGroupName = join("", aws_autoscaling_group.default.*.name)
+    AutoScalingGroupName = join("", aws_autoscaling_group.this.*.name)
   }
 
   alarm_description = "Scale up if CPU utilization is above ${var.cpu_utilization_high_threshold_percent} for ${var.cpu_utilization_high_period_seconds} seconds"
@@ -45,7 +43,7 @@ resource "aws_cloudwatch_metric_alarm" "cpu_high" {
 
 resource "aws_cloudwatch_metric_alarm" "cpu_low" {
   count               = local.autoscaling_enabled ? 1 : 0
-  alarm_name          = "${module.label.id}${var.delimiter}cpu${var.delimiter}utilization${var.delimiter}low"
+  alarm_name          = "${var.name}-cpu-utilization-low"
   comparison_operator = "LessThanOrEqualToThreshold"
   evaluation_periods  = var.cpu_utilization_low_evaluation_periods
   metric_name         = "CPUUtilization"
@@ -55,7 +53,7 @@ resource "aws_cloudwatch_metric_alarm" "cpu_low" {
   threshold           = var.cpu_utilization_low_threshold_percent
 
   dimensions = {
-    AutoScalingGroupName = join("", aws_autoscaling_group.default.*.name)
+    AutoScalingGroupName = join("", aws_autoscaling_group.this.*.name)
   }
 
   alarm_description = "Scale down if the CPU utilization is below ${var.cpu_utilization_low_threshold_percent} for ${var.cpu_utilization_low_period_seconds} seconds"
